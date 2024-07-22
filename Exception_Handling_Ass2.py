3e8ec2f1bb065bd0dee8f0e51d52b4d19331e363
Kunal Kumar <74050098+kunalkumar023@users.noreply.github.com>
2024-07-22 18:16:15 +0530

    Lab2 Added

---
+++ Exception_Handling_Ass2.py
@@ -0,0 +1,40 @@
+# Question-1 
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# try:
+#     print(num1/num2)
+# except ZeroDivisionError as e:
+#     print(e)
+# else:
+#     print("numbers divided successfully!")
+
+# Question -2 
+
+# try:
+#     num=int(input("Enter an integer"))
+# except ValueError as e:
+#     print(e)
+# else:
+#     print("Number is entered")
+
+# Question - 3
+# try:
+#     open("aaa.txt")
+# except FileNotFoundError as e:
+#     print(e)
+# else:
+#     print("File opened!")
+
+# Question - 4
+
+# try:
+#     num1 = int(input("Enter the 1st number: "))
+#     num2 = int(input("Enter the 2nd number: "))
+#     s = str(num1)
+#     result = num2 + s
+# except TypeError :
+#     print("The entered values are not integer.")
+# else:
+#     print("Values are successfully entered!")
