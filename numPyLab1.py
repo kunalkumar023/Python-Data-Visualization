d83027970a36ba79fb938962c59d9a07f293db0e
Kunal Kumar <74050098+kunalkumar023@users.noreply.github.com>
2024-07-26 17:03:11 +0530

    Numpy lab1 and lab 3 added

---
+++ numPyLab1.py
@@ -0,0 +1,33 @@
+import numpy as np
+# Question - 1
+# my_list=[1,2,3,4,5]
+# arr=np.array(my_list)
+# print(arr)
+# print(type(arr))
+
+# Question -2 
+# my_list=[1,2,3,4,5]
+# arr=np.array(my_list)
+# print(arr)
+# print(f"Starting index is {0} and End index is {len(arr)-1}")
+# arr=arr*2
+# print(arr)
+
+# Question - 3
+# Write a NumPy program to create an araay of 10 zeros, 10 ones and 10 fives.
+
+# a=np.zeros(10)
+# print("An array of 10 zeros:")
+# print(a)
+
+# b=np.ones(10)
+# print("An array of 10 ones:")
+# print(b)
+
+# c=np.full(10,5,dtype=float)
+# print("An array of 10 fives:")
+# print(c)
+
+# Question -4 Write a program to create a 3x3 matrix with values ranging from 2 to 10.
+# arr=np.arange(2, 11).reshape(3, 3)
+# print(arr)
